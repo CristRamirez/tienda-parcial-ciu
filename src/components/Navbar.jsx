8448812af7fc577fb5559dc22fe0140800314cb9
Lucas Carrasco <lucasecarrasco1@gmail.com>
2026-06-06 13:24:31 -0300

    feat(navbar): badge con contador de productos del carrito

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,9 @@
 import { Navbar as BSNavbar, Nav, Container } from 'react-bootstrap';
 import { Link, NavLink } from 'react-router-dom';
+import { useCarrito } from '../context/CarritoContext.jsx';
 
 export default function Navbar() {
+  const { cantidadTotal } = useCarrito();
   return (
     <BSNavbar expand="lg" className="navbar-tienda" variant="dark" sticky="top">
       <Container>
@@ -13,7 +15,10 @@ export default function Navbar() {
             <Nav.Link as={NavLink} to="/productos">Productos</Nav.Link>
             <Nav.Link as={NavLink} to="/nosotros">Nosotros</Nav.Link>
             <Nav.Link as={NavLink} to="/contacto">Contacto</Nav.Link>
-            <Nav.Link as={NavLink} to="/carrito">Carrito</Nav.Link>
+            <Nav.Link as={NavLink} to="/carrito">
+              Carrito
+              {cantidadTotal > 0 && <span className="badge-carrito">{cantidadTotal}</span>}
+            </Nav.Link>
           </Nav>
         </BSNavbar.Collapse>
       </Container>
